3b8281e28346d1db3b3a61631f3ee79c17c28812
louis <1358488263@qq.com>
2025-10-27 15:12:03 +0800

    feat:  js实现的单页应用

---
+++ static/js/main.js
@@ -0,0 +1,36 @@
+const contentMap = {
+    home: {
+        title: "首页",
+        file: `home.html`,
+    },
+    articles: {
+        title: "文章",
+        file: `articles.html`,
+    },
+    about: {
+        title: "介绍",
+        file: `about.html`,
+    },
+};
+
+async function loadPage(page) {
+    console.log("Loading page:", page);
+    const { title, file } = contentMap[page];
+    document.title = 'Loiuyis~'+title;
+
+    const res = await fetch(`/pages/${file}`).catch((err) => {
+        console.error("Failed to fetch page:", err);
+    });
+    const html = await res.text().catch((err) => {  
+        console.error("Failed to load page:", err);
+    });
+    document.getElementById("content").innerHTML = html;
+}
+
+loadPage("home");
+
+document.getElementById('content').addEventListener('click', e => {
+  if (e.target.classList.contains('nav-btn')) {
+    loadPage(e.target.dataset.page);
+  }
+});
